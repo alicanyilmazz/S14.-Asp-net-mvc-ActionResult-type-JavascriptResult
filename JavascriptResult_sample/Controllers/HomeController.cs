d36223329f1ddc7fa97fceae5cf416cb462fb7db
alicanyilmazz <alicanyilmazz@users.noreply.github.com>
2019-04-27 21:48:29 +0300

    Add project files.

---
+++ JavascriptResult_sample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JavascriptResult_sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult homepage()
+        {
+            return View();
+        }
+
+        public JavaScriptResult button_click_method()
+        {
+            string js = "function js_button_click() { alert('Hello User');}";
+
+            return JavaScript(js);
+        }
+    }
+}
---
+++ JavascriptResult_sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JavascriptResult_sample.MvcApplication" Language="C#" %>
